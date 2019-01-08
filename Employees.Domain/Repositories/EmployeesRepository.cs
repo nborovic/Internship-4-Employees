ce0d0aa891eacafc1daaa12f3f4e2e1d4545c490
nborovic <nino.borovic@outlook.com>
2019-01-08 23:07:16 +0100

    Implemented repositories

---
+++ Employees.Domain/Repositories/EmployeesRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Employees.Data.Models;
+
+namespace Employees.Domain.Repositories
+{
+    public sealed class EmployeesRepository
+    {
+        public static List<Employee> EmployeesList;
+
+        public List<Employee> GetAll() => EmployeesList;
+    }
+}
---
+++ Employees.Domain/Repositories/ProjectsRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Employees.Data.Models;
+
+namespace Employees.Domain.Repositories
+{
+    public sealed class ProjectsRepository
+    {
+        public static List<Project> ProjectsList;
+
+        public List<Project> GetAll() => ProjectsList;
+    }
+}
