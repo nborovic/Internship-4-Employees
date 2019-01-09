de772b5d12a7bd33af953295e9f504ab665ff403
nborovic <nino.borovic@outlook.com>
2019-01-09 23:37:52 +0100

    Implemented CountOf() method in RelationalRepository

--- Employees.Domain/Repositories/RelationalRepository.cs
+++ Employees.Domain/Repositories/RelationalRepository.cs
@@ -1,21 +1,23 @@
 ﻿using Employees.Data.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Employees.Data.Enums;
 
 namespace Employees.Domain.Repositories
 {
     public class RelationalRepository
     {
-        public static List<Tuple<Project, List<Tuple<Employee, int>>>> ProjectEmployee = new List<Tuple<Project, List<Tuple<Employee, int>>>>();
+        public static List<Tuple<Project, List<Tuple<Employee, int>>>> ProjectEmployeeList = new List<Tuple<Project, List<Tuple<Employee, int>>>>();
 
-        public List<Tuple<Project, List<Tuple<Employee, int>>>> GetAll() => ProjectEmployee;
+        public List<Tuple<Project, List<Tuple<Employee, int>>>> GetAll() => ProjectEmployeeList;
 
         public void Add(Tuple<Project, List<Tuple<Employee, int>>> projectEmployeeToAdd)
         {
-            ProjectEmployee.Add(projectEmployeeToAdd);
+            ProjectEmployeeList.Add(projectEmployeeToAdd);
         }
 
         public void GenerateRelations()
@@ -25,21 +27,37 @@ namespace Employees.Domain.Repositories
             var employeesList = employeesRepo.GetAll();
             var projectsList = projectsRepo.GetAll();
 
-            ProjectEmployee.Add(new Tuple<Project, List<Tuple<Employee, int>>>(projectsList[0], new List<Tuple<Employee, int>>()
+            ProjectEmployeeList.Add(new Tuple<Project, List<Tuple<Employee, int>>>(projectsList[0], new List<Tuple<Employee, int>>()
             {
                 new Tuple<Employee, int>(employeesList[0], 11),
                 new Tuple<Employee, int>(employeesList[1], 8),
                 new Tuple<Employee, int>(employeesList[2], 7)
             }));
-            ProjectEmployee.Add(new Tuple<Project, List<Tuple<Employee, int>>>(projectsList[1], new List<Tuple<Employee, int>>()
+            ProjectEmployeeList.Add(new Tuple<Project, List<Tuple<Employee, int>>>(projectsList[1], new List<Tuple<Employee, int>>()
             {
                 new Tuple<Employee, int>(employeesList[1], 5),
                 new Tuple<Employee, int>(employeesList[2], 11)
             }));
-            ProjectEmployee.Add(new Tuple<Project, List<Tuple<Employee, int>>>(projectsList[2], new List<Tuple<Employee, int>>()
+            ProjectEmployeeList.Add(new Tuple<Project, List<Tuple<Employee, int>>>(projectsList[2], new List<Tuple<Employee, int>>()
             {
                 new Tuple<Employee, int>(employeesList[0], 8),
             }));
+        }
+
+        public int CountOf(Project selectedProject, Role role)
+        {
+            var counter = 0;
+
+            foreach (var projectEmployee in ProjectEmployeeList)
+                if (selectedProject.Equals(projectEmployee.Item1))
+                {
+                    foreach (var employee in projectEmployee.Item2)
+                        if (employee.Item1.Role == role)
+                            counter++;
+                    break;
+                }
+
+            return counter;
         }
     }
 }
